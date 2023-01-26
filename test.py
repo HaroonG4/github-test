692c9efffa11936e6e8894a2382dbb6443e1d927
Haroon Goraya <=>
2023-01-26 12:46:42 -0500

    message 4

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 x = int(input("Enter a Number: "))
 y = int(input("Enter a Number: "))
 
-if y%x == 0  == 0:
+if y%x == 0 :
     print(f'{x} is a multiple of {y}')
 else:
     print(f'{x} is not a multiple of {y}')
